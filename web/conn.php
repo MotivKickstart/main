486ca820d75a74730f4d14078882d3be6dc8b8a2
Jelle <jellevdhelm1@gmail.com>
2023-11-26 16:27:45 +0100

    start web

---
+++ web/conn.php
@@ -0,0 +1,20 @@
+<?
+
+$dataArray = array();
+$database = "mydb";
+$user = "myuser";
+$password = "password";
+$host = "mysql";
+
+$conn = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);
+$Resultsstmt = $conn->prepare("SELECT * FROM users");
+
+$Resultsstmt->execute();
+
+$Results = [];
+
+while ($result = $Resultsstmt->fetch(PDO::FETCH_ASSOC)) {
+    $Results[] = $result;
+}
+
+echo json_encode(['Results' => $Results], JSON_NUMERIC_CHECK);
---
+++ web/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Motiv</title>
+
+    <script src="scripts.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <header>
+        <div style="display: none;">
+            <?php require_once('conn.php') ?>
+        </div>
+        <h1>Dashboard</h1>
+    </header>
+
+    <main>
+        <p>badabing badaboem</p>
+    </main>
+
+</body>
+
+</html>
