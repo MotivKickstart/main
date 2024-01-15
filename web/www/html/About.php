16027218ef4a4933ff03db59973191b07d98ddeb
Kasper Beljaars <kasperbeljaars@hotmail.com>
2024-01-15 15:22:58 +0100

    Created about us page

--- web/www/html/About.php
+++ web/www/html/About.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About us</title>
+    <link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
     <div class="navbar">
@@ -33,7 +34,24 @@
             <?php } ?>
         </ul>
     </div>
-
+    <div class="container">
+        <h1 class="title title-l title-center">About us</h1>
+    <p>
+        Welcome to Motiv, where your health journey is our priority. At Motiv, we believe in empowering individuals to make conscious choices for a healthier and more balanced life. Our team of dedicated professionals, driven by a passion for health and innovation, has created Motiv as a comprehensive ecosystem to guide you towards your wellness goals.
+    </p>
+    <p>
+        We understand that health is personal, and each individual has unique needs. That's why Motiv offers more than just measurement data; it provides insights, support, and inspiration for your specific health journey. Whether you're aiming for weight loss, muscle building, or overall well-being, Motiv stands by your side.
+    </p>
+    <p>
+        Our mission is clear: to push the boundaries of health management by combining advanced technology and user-friendly designs. At Motiv, we aspire to be not just a product provider but a partner on your path to a healthier and happier life.
+    </p>
+    <p>
+        Thank you for being part of the Motiv community. Together, we embark on a journey of discovery, every step of the way.
+    </p>
+    <p>
+        With healthy regards,<br>
+        The Movit Team
+    </p>
     <script src="nav.js"></script>
 </body>
 </html>
