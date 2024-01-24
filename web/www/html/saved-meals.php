7eb4e6a3990e8464d3e3f0015cb9e7efcb601739
Kasper Beljaars <kasperbeljaars@hotmail.com>
2024-01-24 16:16:23 +0100

    Created recipies search with local storage

--- web/www/html/saved-meals.php
+++ web/www/html/saved-meals.php
@@ -48,33 +48,19 @@
             </div>
         </div>
         <ul class="nav-list">
-            <li><a href="saved-meals.php">Saved Meals</a></li>
-            <li><a href="ingredients.php">Ingredients</a></li>
-            <li><a href="recipes.php">Recipes</a></li>
             <li><a href="Dispenser.php">Dispenser</a></li>
+            <li><a href="ingredients.php">Meal tracking</a></li>
+            <li><a href="recipes.php">Recipes</a></li>
+            <li><a href="saved-meals.php">Saved Meals</a></li>
             <li><a href="About.php">About</a></li>
             <?php if (isset($_SESSION['loggedin'])) { ?>
                 <li class="nav-list__login"><a href="/php/logout.php">Logout</a></li>
             <?php } ?>
         </ul>
     </div>
-    <div id="saved-meals-container">
-        <h2>Saved Meals</h2>
-        <table>
-            <thead>
-            <tr>
-                <th>Meal Name</th>
-                <th>Ingredients</th>
-                <th>Calories (kcal)</th>
-                <th>Fat (g)</th>
-                <th>Protein (g)</th>
-                <th>Weight (g)</th>
-            </tr>
-            </thead>
-            <tbody id="saved-meals-table-body">
-            <!-- Saved meal details will be added here dynamically -->
-            </tbody>
-        </table>
+    <div class="container padding">
+        <h1 class="title title-l title-center">Saved Meals</h1>
+        <div id="recipe-container" class="padding"></div>
         <button class="button button--primary" onclick="clearLocalStorage()">Clear Local Storage</button> <!-- Added button to trigger clearLocalStorage -->
     </div>
     <script src="scripts/scriptSavedMeals.js"></script>
