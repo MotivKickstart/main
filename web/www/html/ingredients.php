ba04743c6d9a949738575c2db3d3ce4458a17095
Kasper Beljaars <kasperbeljaars@hotmail.com>
2024-01-24 16:15:21 +0100

    Created meal tracking with local host

--- web/www/html/ingredients.php
+++ web/www/html/ingredients.php
@@ -1,10 +1,9 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Motiv | Ingredients</title>
     <link rel="stylesheet" href="/css/main.css">
     <link rel="apple-touch-icon" sizes="57x57" href="/gfx/favicon/apple-icon-57x57.png">
@@ -28,14 +27,12 @@
 
 <body>
     <div style="display: block;">
-        <?php require_once('conn.php')?>
+        <?php require_once('php/conn.php')?>
     </div>
     <div class="navbar">
         <div class="logo">
             <a href="index.php">
-                <a href="index.php">
                 <img src="/gfx/logo.svg" alt="Logo">
-            </a>
             </a>
         </div>
         <div class="menu-icon-holder">
@@ -54,54 +51,29 @@
             </div>
         </div>
         <ul class="nav-list">
-            <li><a href="saved-meals.php">Saved Meals</a></li>
-            <li><a href="ingredients.php">Ingredients</a></li>
-            <li><a href="recipes.php">Recipes</a></li>
             <li><a href="Dispenser.php">Dispenser</a></li>
+            <li><a href="ingredients.php">Meal tracking</a></li>
+            <li><a href="recipes.php">Recipes</a></li>
+            <li><a href="saved-meals.php">Saved Meals</a></li>
             <li><a href="About.php">About</a></li>
             <?php if (isset($_SESSION['loggedin'])) { ?>
                 <li class="nav-list__login"><a href="/php/logout.php">Logout</a></li>
             <?php } ?>
         </ul>
     </div>
-    
-    <div class="form">
-        <label for="recipe">Enter Food Items:</label>
-        <textarea id="recipe" placeholder="Enter your food items, each item on a new line"></textarea>
-        <button class="button button--primary" onclick="analyzeNutrition()">Analyze Nutrition</button>
+    <div class="container">
+        <h1 class="title title-l title-center">Meal Tracking</h1>
+        <div class="form">
+            <label for="recipe">Enter Food Items:</label>
+            <textarea id="recipe" placeholder="Enter your food items, each item on a new line"></textarea>
+            <button class="button button--primary" onclick="analyzeNutrition()">Add meal</button>
+        </div>
+        <div id="result" class="padding"></div>
+        <h2 class="title title-m">Total today</h2>
+        <div id="total"></div>
     </div>
-    <div id="result-container">
-        <!-- Results will be displayed here dynamically -->
-        <table>
-            <thead>
-                <tr>
-                    <th>Ingredient</th>
-                    <th>Calories (kcal)</th>
-                    <th>Fat (g)</th>
-                    <th>Protein (g)</th>
-                    <th>Weight (g)</th>
-                </tr>
-            </thead>
-            <tbody id="result-table-body">
-                <!-- Ingredient details will be added here dynamically -->
-            </tbody>
-            <tfoot>
-                <tr id="total-row">
-                    <td>Total</td>
-                    <td id="total-calories"></td>
-                    <td id="total-fat"></td>
-                    <td id="total-protein"></td>
-                    <td id="total-weight"></td>
-                </tr>
-                <tr id="total-nutrients">
-                    <td colspan="5" id="overview-label"></td>
-                </tr>
-            </tfoot>
-        </table>
-        <button class="button button--primary" onclick="saveMeal()">Save meal +</button>
-    </div>
-    <script src="main.js"></script>
-    <script src="nav.js"></script>
+    <script src="/scripts/main.js"></script>
+    <script src="/scripts/nav.js"></script>
 </body>
 
 </html>
--- web/www/html/scripts/main.js
+++ web/www/html/scripts/main.js
@@ -19,6 +19,7 @@ async function analyzeNutrition() {
 
     const data = await response.json();
     displayResults(data.ingredients, data.totalNutrients);
+    saveMeal(data.ingredients, data.totalNutrients);
     console.log(data);
   } catch (error) {
     console.error('Error:', error);
@@ -26,19 +27,19 @@ async function analyzeNutrition() {
 }
 
 function displayResults(ingredients, totalNutrients) {
-  const resultTableBody = document.getElementById('result-table-body');
-  const totalCaloriesElement = document.getElementById('total-calories');
-  const totalFatElement = document.getElementById('total-fat');
-  const totalProteinElement = document.getElementById('total-protein');
-  const totalWeightElement = document.getElementById('total-weight');
-  const overviewLabelElement = document.getElementById('overview-label');
+  const result = document.getElementById('result');
+  const resultCard = document.createElement('div');
+  resultCard.classList.add('card');
+  const ingredientsElement = document.createElement('ul');
+  const totalCaloriesElement = document.createElement('p');
+  totalCaloriesElement.classList.add('label');
+  const totalProteinElement = document.createElement('p');
+  totalProteinElement.classList.add('label');
 
-  resultTableBody.innerHTML = ''; // Clear previous results
+  result.innerHTML = '';
 
   let totalCalories = 0;
-  let totalFat = 0;
   let totalProtein = 0;
-  let totalWeight = 0;
 
   ingredients.forEach(ingredient => {
     const { food, nutrients, weight } = ingredient.parsed[0];
@@ -49,73 +50,53 @@ function displayResults(ingredients, totalNutrients) {
     const ingredientWeight = parseFloat(weight).toFixed(2);
 
     totalCalories += parseFloat(calories);
-    totalFat += parseFloat(fat);
     totalProtein += parseFloat(protein);
-    totalWeight += parseFloat(ingredientWeight);
 
-    const row = document.createElement('tr');
-    row.innerHTML = `<td>${name}</td><td>${calories}</td><td>${fat}</td><td>${protein}</td><td>${ingredientWeight}</td>`;
-    resultTableBody.appendChild(row);
+    ingredientsElement.innerHTML += `<li>${ingredientWeight}g ${name}</li>`;
   });
 
   // Display total values
-  totalCaloriesElement.textContent = totalCalories.toFixed(2);
-  totalFatElement.textContent = totalFat.toFixed(2);
-  totalProteinElement.textContent = totalProtein.toFixed(2);
-  totalWeightElement.textContent = totalWeight.toFixed(2);
+  totalCaloriesElement.innerHTML = `Total calories: ${totalCalories.toFixed(2)}g`;
+  totalProteinElement.innerHTML = `Total protein: ${totalProtein.toFixed(2)}g`;
 
-  // Display overview label
-  const overviewText = `
-    Total Fat ${totalNutrients.FAT.quantity.toFixed(2)} ${totalNutrients.FAT.unit} <br>
-    Saturated Fat ${totalNutrients.FASAT.quantity.toFixed(2)} ${totalNutrients.FASAT.unit} <br>
-    Trans Fat ${totalNutrients.FATRN.quantity.toFixed(2)} ${totalNutrients.FATRN.unit} <br>
-    Cholesterol ${totalNutrients.CHOLE.quantity.toFixed(2)} ${totalNutrients.CHOLE.unit} <br>
-    Sodium ${totalNutrients.NA.quantity.toFixed(2)} ${totalNutrients.NA.unit} <br>
-    Total Carbohydrate ${totalNutrients.CHOCDF.quantity.toFixed(2)} ${totalNutrients.CHOCDF.unit} <br>
-    Dietary Fiber ${totalNutrients.FIBTG.quantity.toFixed(2)} ${totalNutrients.FIBTG.unit} <br>
-    Total Sugars ${totalNutrients.SUGAR.quantity.toFixed(2)} ${totalNutrients.SUGAR.unit} <br>
-    Protein ${totalNutrients.PROCNT.quantity.toFixed(2)} ${totalNutrients.PROCNT.unit} <br>
-    Vitamin D ${totalNutrients.VITD.quantity.toFixed(2)} ${totalNutrients.VITD.unit} <br>
-    Calcium ${totalNutrients.CA.quantity.toFixed(2)} ${totalNutrients.CA.unit} <br>
-    Iron ${totalNutrients.FE.quantity.toFixed(2)} ${totalNutrients.FE.unit} <br>
-    Potassium ${totalNutrients.K.quantity.toFixed(2)} ${totalNutrients.K.unit}
-  `;
-  overviewLabelElement.innerHTML = overviewText;
+  // Append elements to the result card
+  resultCard.appendChild(ingredientsElement);
+  resultCard.appendChild(totalCaloriesElement);
+  resultCard.appendChild(totalProteinElement);
+  result.appendChild(resultCard);
 }
 
-function saveMeal() {
-  const ingredientRows = document.querySelectorAll("#result-table-body tr");
-  const ingredients = Array.from(ingredientRows).map(row => ({
-    name: row.cells[0].textContent,
-    calories: parseFloat(row.cells[1].textContent),
-    fat: parseFloat(row.cells[2].textContent),
-    protein: parseFloat(row.cells[3].textContent),
-    weight: parseFloat(row.cells[4].textContent)
-  }));
+function saveMeal(ingredients, totalNutrients) {
+  const meal = {
+    ingredients: ingredients,
+    totalNutrients: totalNutrients,
+  };
 
-  const totalCalories = ingredients.reduce((sum, ingredient) => sum + ingredient.calories, 0);
-  const totalFat = ingredients.reduce((sum, ingredient) => sum + ingredient.fat, 0);
-  const totalProtein = ingredients.reduce((sum, ingredient) => sum + ingredient.protein, 0);
-  const totalWeight = ingredients.reduce((sum, ingredient) => sum + ingredient.weight, 0);
+  let meals = JSON.parse(localStorage.getItem('totals')) || [];
+  meals = Array.isArray(meals) ? meals : [];
+  meals.push(meal);
 
-  const mealData = {
-    ingredients: ingredients.map(ingredient => `${ingredient.weight}g ${ingredient.name}`),
-    calories: totalCalories.toFixed(2),
-    fat: totalFat.toFixed(2),
-    protein: totalProtein.toFixed(2),
-    weight: totalWeight.toFixed(2)
-  };  
-
-  // Retrieve existing meals from local storage
-  const existingMeals = JSON.parse(localStorage.getItem('meals')) || [];
-
-  // Add the current meal to the existing meals array
-  existingMeals.push(mealData);
-
-  // Save updated meals in local storage
-  localStorage.setItem('meals', JSON.stringify(existingMeals));
-
-  displaySavedMeals();
+  localStorage.setItem('totals', JSON.stringify(meals));
+  displayTotal();
 }
+
+function displayTotal() {
+  const meals = JSON.parse(localStorage.getItem('totals'));
+  let totalCalories = 0;
+  let totalProtein = 0;
+
+  if (Array.isArray(meals)) {
+    meals.forEach(meal => {
+      const { totalNutrients } = meal;
+      totalCalories += parseFloat(totalNutrients.ENERC_KCAL.quantity);
+      totalProtein += parseFloat(totalNutrients.PROCNT.quantity);
+    });
+  }
+
+  const totalElement = document.getElementById('total');
+  totalElement.innerHTML = `Total calories: ${totalCalories.toFixed(2)}g<br> Total protein: ${totalProtein.toFixed(2)}g`;
+}
+
+displayTotal();
 
 
