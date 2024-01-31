22c486030328486df59f17f1061415c0d81fa678
Kasper Beljaars <kasperbeljaars@hotmail.com>
2024-01-31 12:12:29 +0100

    Update index.php

--- web/www/html/index.php
+++ web/www/html/index.php
@@ -64,14 +64,6 @@
             <li><a href="About.php">About</a></li>
             <!-- <li><a href="loginForm.php">login</a></li> temp links for testing
             <li><a href="registerForm.php">register</a></li> temp links for testing -->
-            <?php
-            if (isset($_SESSION['loggedin'])) {
-                echo "<li><a href=\"php/logout.php\">Logout</a></li>";
-            } else {
-                echo "<li><a href=\"loginForm.php\">login</a></li>";
-                echo "<li><a href=\"registerForm.php\">register</a></li>";
-            }
-            ?>
         </ul>
     </div>
 
